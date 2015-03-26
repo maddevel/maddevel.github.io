0e0479d6868c6b98668d1b3bebca129b34bac5c3
Dmitry Akimov <deemidroll@gmail.com>
2015-03-26 11:25:00 +0300

    upd webapp

---
+++ webapp/js/controllers.js
@@ -0,0 +1,9 @@
+angular.module('starter.controllers', [])
+
+.controller('AboutCtrl', function($scope) {})
+
+.controller('TeamCtrl', function($scope) {})
+
+.controller('WorkCtrl', function($scope) {})
+
+.controller('ProjectsCtrl', function($scope) {})
